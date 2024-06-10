c7ed649442dcd96a63605ff29d913c40bcc3b19d
giojano1 <giorgi.janelidze64@gmail.com>
2024-06-11 03:42:54 +0400

    feature/swiper
    bug swiper navigation button display

--- js/main.js
+++ js/main.js
@@ -3,3 +3,30 @@ const btn = document.querySelector(".menu_btn");
 btn.addEventListener("click", () => {
   btn.classList.toggle("open");
 });
+
+const swiper = new Swiper(".feedback_swiper", {
+  direction: "horizontal",
+  loop: true,
+
+  navigation: {
+    nextEl: ".button-next",
+    prevEl: ".button-prev",
+  },
+
+  speed: 2000,
+  effect: "slide",
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    700: {
+      slidesPerView: 2,
+    },
+    1000: {
+      slidesPerView: 3,
+    },
+    1300: {
+      slidesPerView: 4,
+    },
+  },
+});
